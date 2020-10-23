41e0dac82b937798748482799cb2a957899b7ed4
Viktoriya <tory-tory-00@mail.ru>
2020-10-23 20:49:41 +0300

    SDFSDFSD

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,25 +1,24 @@
-from framework.consts import DIR_STATIC
 import mimetypes
+
+from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
     file_names = {
-        '/styles.css': "styles.css",
-         "/logo.jpeg":"123.jpg",
-
-        }
+        "/styles.css": "styles.css",
+        "/logo.jpeg": "123.jpg",
+    }
     status = "200 OK"
-    file_name = file_names.get(url,"payload.html")
+    file_name = file_names.get(url, "payload.html")
     headers = {
-            "Content-type": mimetypes.guess_type(file_name)[0],
-        }
+        "Content-type": mimetypes.guess_type(file_name)[0],
+    }
     payload = read_static(file_name)
     start_response(status, list(headers.items()))
 
     yield payload
-
 
 
 def read_static(file_name: str) -> bytes:
