d9e8f4c23461966ce57b4989b9f2c29731bb9963
Viktoriya <tory-tory-00@mail.ru>
2020-10-23 18:57:51 +0300

    SDFSDFSD

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -2,14 +2,14 @@ from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
-    url = environ['PATH_INFO']
-    if url == '/styles.css':
+    url = environ["PATH_INFO"]
+    if url == "/styles.css":
         status = "200 OK"
         headers = {
             "Content-type": "text/css",
         }
         payload = read_from_styles_css()
-        start_response(status,list(headers.items()))
+        start_response(status, list(headers.items()))
 
         yield payload
 
@@ -21,7 +21,6 @@ def application(environ, start_response):
         payload = read_from_logo_jpeg()
         start_response(status, list(headers.items()))
         yield payload
-
 
     else:
         status = "200 OK"
@@ -46,21 +45,15 @@ def read_from_index_html():
 
 
 def read_from_styles_css():
-        path=DIR_STATIC / "styles.css"
-        with path.open("r") as fp:
-            payload = fp.read()
-        payload = payload.encode()
-        return payload
+    path = DIR_STATIC / "styles.css"
+    with path.open("r") as fp:
+        payload = fp.read()
+    payload = payload.encode()
+    return payload
+
 
 def read_from_logo_jpeg():
-        path=DIR_STATIC / "123.jpg"
-        with path.open("rb") as fp:
-            payload = fp.read()
-        return payload
-
-
-
-
-
-
-
+    path = DIR_STATIC / "123.jpg"
+    with path.open("rb") as fp:
+        payload = fp.read()
+    return payload
