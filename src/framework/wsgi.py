65e07f481dcb18ce9b2cbfbdb811ca3161f9641b
Viktoriya <tory-tory-00@mail.ru>
2020-11-02 23:43:36 +0300

    123

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -2,17 +2,18 @@ from handlers import handle_index
 from handlers import handle_logo
 from handlers import handle_styles
 from handlers import system_handlers
-handlers = {"/": handle_index,
-            "/styles.css": handle_styles,
-            "/logo.jpeg": handle_logo
-            }
+
+handlers = {"/": handle_index, "/styles.css": handle_styles, "/logo.jpeg": handle_logo}
+
 
 def application(environ, start_response):
 
     url = environ["PATH_INFO"]
+
+    handler = handlers.get(url, system_handlers.handle_404)
+
     status, headers, payload = handler(environ)
+
     start_response(status, list(headers.items()))
 
     yield payload
-
- 
