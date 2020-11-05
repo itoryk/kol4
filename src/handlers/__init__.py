f21de48036462120f823cac465accd83bb0d844c
Viktoriya <tory-tory-00@mail.ru>
2020-11-05 22:46:02 +0300

    change styles for not_found

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,4 +1,11 @@
+from framework.types import RequestT
+from framework.types import ResponseT
+
 from . import system_handlers
 from .index import handle_index
 from .logo import handle_logo
 from .styles import handle_styles
+
+
+def handle_hello():
+    return None
