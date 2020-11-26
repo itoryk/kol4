191b8e25987f07aeab51fb67eb006e5b60c4a907
Viktoriya <tory-tory-00@mail.ru>
2020-11-27 00:52:09 +0300

    base.html django_1

--- src/applications/landing/views.py
+++ src/applications/landing/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    result = render(request, "landing/_base.html")
+    result = render(request, "landing/payload.html")
 
     return HttpResponse(result)
